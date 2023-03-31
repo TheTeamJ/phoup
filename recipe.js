be0dd008e14e2ad0b60283ad0e8e70b86e2bd468
daiiz <da.iz.mg@gmail.com>
2023-04-01 03:26:49 +0900

    一旦

--- recipe.js
+++ recipe.js
@@ -1,7 +1,10 @@
 const { Input, Output } = require("./config");
 
-const recipe = [[Input.files.Instagram, Output.gyazo]];
+const recipes = [
+  // [Input[], Output[]], [Input, Output] の形式で記述する
+  [Input.files.Instagram, Output.gyazo],
+];
 
 module.exports = {
-  recipe,
+  recipes,
 };
--- src/lib/librecipe.js
+++ src/lib/librecipe.js
@@ -1,4 +1,31 @@
-function parseRecipe(recipe) {}
+const { config } = require("./config");
+
+const createRecipeList = (recipe) => {
+  const recipeList = [];
+  if (Array.isArray(recipe)) {
+    recipeList.push(...recipe);
+  } else {
+    recipeList.push(recipe);
+  }
+  return recipeList;
+};
+
+/**
+ * recipeを満たすファイルパスとOutput情報を返す
+ * @param {*} recipe
+ * @param {boolean} applyTransform transformを適用するかどうか
+ */
+function parseRecipe(recipe, applyTransform = false) {
+  const recipeList = createRecipeList(recipe);
+
+  const targetPaths = [];
+
+  const res = {
+    targetPaths,
+    output: recipe[1],
+  };
+  return res;
+}
 
 module.exports = {
   parseRecipe,

18f9b82fafcecd08e0bd10b955e6ba0148894995
daiiz <da.iz.mg@gmail.com>
2023-04-01 03:06:28 +0900

    実験

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+
 const config = {
   InputBasePath: "./raw",
   Input: {
-    files: {},
+    files: {
+      Instagram: [
+        {
+          pattern:
+            /^IMG_(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})_(?<h>\d{2})(?<m>\d{2})(?<s>\d{2})_\d+\.jpe?g/,
+          transform: [],
+        },
+      ],
+    },
   },
   Output: {
-    gyazo: {},
+    gyazo: {
+      token: process.env.GYAZO_ACCESS_TOKEN,
+    },
   },
 };
 

8f933c05281903e2a71d663197ff65b64fa867e5
daiiz <da.iz.mg@gmail.com>
2023-04-01 12:59:48 +0900

    wrapInput

---
+++ src/lib/libconfig.js
@@ -0,0 +1,26 @@
+// input object内部を辿って、keyを生やしていく
+// keyは、ルートからの階層を含めたキー情報
+// 例 {files: {a: {b: [1]}, {bb: 2}}}
+// -> { files: { a: { b: [1], key: "files.a.b" }, { bb: 2, key: "files.a.bb" }, key: "files.a"}, key: "files"}
+function wrapInput(input, key = "") {
+  if (typeof input === "object") {
+    if (Array.isArray(input)) {
+      return;
+    }
+    const objKeys = Object.keys(input);
+    objKeys.forEach((k) => {
+      const newKey = key ? `${key}.${k}` : k;
+      input[k]._ = newKey;
+      wrapInput(input[k], newKey);
+    });
+  }
+}
+
+function wrapConfig(config) {
+  const input = wrapInput(config.Input);
+  return config;
+}
+
+module.exports = {
+  wrapConfig,
+};

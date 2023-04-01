ec2749ab8883591560570e3a92e3aed93ceacc55
daiiz <da.iz.mg@gmail.com>
2023-04-01 13:02:10 +0900

    構造が安定してきた

--- config.js
+++ config.js
@@ -1,26 +1,32 @@
 require("dotenv").config();
+const { wrapConfig } = require("./src/lib/libconfig");
 
 const config = {
   InputBasePath: "./raw",
   Input: {
     files: {
-      Instagram: [
-        {
-          app: "Instagram",
-          pattern:
-            /^IMG_(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})_(?<h>\d{2})(?<m>\d{2})(?<s>\d{2})_\d+\.jpe?g/,
-          transform: [],
-        },
-      ],
+      Instagram: {
+        settings: [
+          {
+            app: "Instagram",
+            pattern:
+              /^IMG_(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})_(?<h>\d{2})(?<m>\d{2})(?<s>\d{2})_\d+\.jpe?g/,
+            transform: [],
+          },
+        ],
+      },
+      Demo: {},
     },
   },
   Output: {
     gyazo: {
-      token: process.env.GYAZO_ACCESS_TOKEN,
+      // token: process.env.GYAZO_ACCESS_TOKEN,
     },
   },
 };
 
+console.log("...", JSON.stringify(wrapConfig(config)));
+
 module.exports = {
-  config,
+  config: wrapConfig(config),
 };

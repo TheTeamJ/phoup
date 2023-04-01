80c2f53ec27bdd240846c52e2c33de006da2026b
daiiz <da.iz.mg@gmail.com>
2023-04-01 14:03:25 +0900

    将来に向けて

--- src/lib/libfile.js
+++ src/lib/libfile.js
@@ -6,31 +6,35 @@ const { promisify } = require("util");
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
 
+const addFile = (file, filePath, fileStat, targetFiles, pattern, timezone) => {
+  // ファイル名fileに対して名前付き正規表現patternをマッチさせて、日時情報を取得する
+  // 取得に失敗したら例外を投げる
+  const match = file.match(pattern);
+  if (!match) {
+    throw new Error("Invalid file name");
+  }
+  const { groups } = match;
+  const { year, month, day, h, m, s, unixtime } = groups;
+
+  targetFiles.push({
+    name: file,
+    path: filePath,
+    size: fileStat.size,
+    dateInfo: createDateStr(groups, timezone),
+    // modifiedAt: fileStat.mtime,
+  });
+};
+
 async function findFiles(targetDir, pattern, timezone) {
   const targetFiles = [];
   const files = await readdir(targetDir);
   for (const file of files) {
-    // TODO: 再帰的に探す
     const filePath = path.join(targetDir, file);
     const fileStat = await stat(filePath);
     if (fileStat.isFile() && pattern.test(file)) {
-      // ファイル名fileに対して名前付き正規表現patternをマッチさせて、日時情報を取得する
-      // 取得に失敗したら例外を投げる
-      const match = file.match(pattern);
-      if (!match) {
-        throw new Error("Invalid file name");
-      }
-      const { groups } = match;
-      const { year, month, day, h, m, s, unixtime } = groups;
-
-      targetFiles.push({
-        name: file,
-        path: filePath,
-        size: fileStat.size,
-        dateInfo: createDateStr(groups, timezone),
-        // modifiedAt: fileStat.mtime,
-      });
+      addFile(file, filePath, fileStat, targetFiles, pattern, timezone);
     }
+    // TODO: 再帰的に探す
   }
   return targetFiles;
 }

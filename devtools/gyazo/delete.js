8fb02d6340b010b1bf9c7c307628f1c2fb4cf881
daiiz <da.iz.mg@gmail.com>
2023-04-02 22:22:59 +0900

    devtools

--- devtools/gyazo/delete.js
+++ devtools/gyazo/delete.js
@@ -1,1 +1,34 @@
-// TODO
+const fetch = require("node-fetch");
+const { loadLogLines } = require("../../src/lib/liblog");
+require("dotenv").config();
+
+// https://gyazo.com/api/docs/image
+const API_BASE_URL = "https://api.gyazo.com/api/images";
+
+async function deleteGyazoImages(imageIds = []) {
+  const { GYAZO_ACCESS_TOKEN } = process.env;
+  if (!GYAZO_ACCESS_TOKEN) {
+    throw new Error("GYAZO_ACCESS_TOKEN is empty");
+  }
+  for (const imageId of imageIds) {
+    const apiUrl = `${API_BASE_URL}/${imageId}?access_token=${GYAZO_ACCESS_TOKEN}`;
+    const res = await fetch(apiUrl, {
+      method: "DELETE",
+    });
+    console.log("deleteGyazoImages:", res.status, imageId);
+  }
+}
+
+async function main() {
+  // Example:
+  const ids = await loadLogLines(
+    "gyazo",
+    true,
+    "2023-04-02",
+    "2023-04-02_220408"
+  );
+  console.log("targetId:", ids);
+  await deleteGyazoImages(ids);
+}
+
+main();

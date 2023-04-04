71b02bec4b1b39a0dcecd9d6f6dcaaf2506bde4d
daiiz <da.iz.mg@gmail.com>
2023-04-04 18:08:14 +0900

    .

--- src/transform/File.js
+++ src/transform/File.js
@@ -3,7 +3,7 @@ const { DateTime } = require("luxon");
 const { createDateInfo } = require("../lib/libdate");
 const fsPromises = fs.promises;
 
-const apply = async (file) => {
+const applyOverwriteDateByFileMetadata = async (file) => {
   console.log("[transform/overwriteDateByFileMetadata]", file.path);
 
   // ファイルの作成日を取得
@@ -23,10 +23,23 @@ const apply = async (file) => {
   return file;
 };
 
+const applyOverwriteDescByFileMetadata = async (file) => {
+  console.log("[transform/overwriteDescByFileMetadata]", file.path);
+  throw new Error("Not implemented.");
+};
+
+async function overwriteDescByFileMetadata(files = []) {
+  const res = [];
+  for (const file of files) {
+    res.push(await applyOverwriteDescByFileMetadata(file));
+  }
+  return res;
+}
+
 async function overwriteDateByFileMetadata(files = []) {
   const res = [];
   for (const file of files) {
-    res.push(await apply(file));
+    res.push(await applyOverwriteDateByFileMetadata(file));
   }
   return res;
 }
